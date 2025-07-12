d00c032e6292a3b447ce63d1de36901209b8fbe6
nmanhas2 <manhasnubal@gmail.com>
2025-07-12 14:03:32 -0600

    added buttons to clear and change color and erase

--- script.js
+++ script.js
@@ -1,13 +1,66 @@
-let NUM_GRID_ROWS = 64;
-let NUM_GRID_COLUMNS = NUM_GRID_ROWS;
+let NUM_GRID_ROWS = 0;
+let NUM_GRID_COLUMNS = 0;
+
+let random_color = false;
+let eraser = false;
+
 const grid_container = document.querySelector(".grid_container");
+const grid_size = document.querySelector("#grid_size");
+const grid_color_mode = document.querySelector("#grid_color_mode");
+const grid_clear = document.querySelector("#grid_clear");
+const grid_eraser = document.querySelector("#grid_eraser");
+
+grid_container.addEventListener("mouseenter", (e) =>{
+    if (!e.target.classList.contains("grid_column")) return;
+    
+    if(eraser){
+        e.target.style.backgroundColor = "white";
+        return;
+    }
+    
+    if(random_color){
+        e.target.style.backgroundColor = GetRandomColor();
+    }else{
+        e.target.style.backgroundColor = "black";
+    }
+
+}, true);
+
+grid_eraser.addEventListener("click", () => {
+    eraser = true;
+});
+
+grid_clear.addEventListener("click", () => {
+    SetGridSize();
+});
+
+grid_size.addEventListener("input", (e) =>{
+    SetGridSize();
+});
+
+grid_color_mode.addEventListener("click", () => {
+    random_color = !random_color;
+    eraser = false;
+    grid_color_mode.textContent = random_color ? "Random Color" : "Black";
+});
+
+function GetRandomColor() {
+  // Generate a random hex color
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for(let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function GenerateGrid(){
+
+    grid_container.innerHTML = "";
+
     const grid_container_rectangle = grid_container.getBoundingClientRect();
     const column_height = grid_container_rectangle.height / NUM_GRID_ROWS;
     const column_width = grid_container_rectangle.width / NUM_GRID_ROWS;
-    console.log(column_height);
-    console.log(column_width);
 
     for(let i = 0; i < NUM_GRID_ROWS; i++){
         const row = document.createElement("div");
@@ -28,10 +81,6 @@ function GenerateGrid(){
             column.style.width = `${column_width}px`
             column.style.boxSizing = "border-box";
 
-            column.addEventListener("mouseenter", (e) =>{
-                e.target.style.backgroundColor = "black";
-            });
-
             row.appendChild(column);
         }
 
@@ -39,4 +88,12 @@ function GenerateGrid(){
     }
 }
 
-GenerateGrid();
+function SetGridSize(){
+    NUM_GRID_COLUMNS = grid_size.value;
+    NUM_GRID_ROWS = grid_size.value;
+
+    GenerateGrid();
+}
+
+SetGridSize();
+
