8915ddacff57e9666521f850e529012cf4925330
nmanhas2 <manhasnubal@gmail.com>
2025-07-12 13:16:23 -0600

    added mouseenter event and borders to the columns

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const NUM_GRID_ROWS = 16;
-const NUM_GRID_COLUMNS = 16;
+let NUM_GRID_ROWS = 64;
+let NUM_GRID_COLUMNS = NUM_GRID_ROWS;
 const grid_container = document.querySelector(".grid_container");
 
-
 function GenerateGrid(){
+    const grid_container_rectangle = grid_container.getBoundingClientRect();
+    const column_height = grid_container_rectangle.height / NUM_GRID_ROWS;
+    const column_width = grid_container_rectangle.width / NUM_GRID_ROWS;
+    console.log(column_height);
+    console.log(column_width);
+
     for(let i = 0; i < NUM_GRID_ROWS; i++){
         const row = document.createElement("div");
+
+        row.style.display = "flex";
+        row.style.justifyContent = "center";
+
         row.classList.add("grid_row");
         row.id = `row_${i}`;
 
         for(let j = 0; j < NUM_GRID_COLUMNS; j++){
             const column = document.createElement("div");
+
             column.classList.add("grid_column");
             column.id = `row_${i}_column${j}`;
-            column.textContent = j;
+            column.style.border = "2px solid black"
+            column.style.height = `${column_height}px`
+            column.style.width = `${column_width}px`
+            column.style.boxSizing = "border-box";
+
+            column.addEventListener("mouseenter", (e) =>{
+                e.target.style.backgroundColor = "black";
+            });
+
             row.appendChild(column);
         }
 
